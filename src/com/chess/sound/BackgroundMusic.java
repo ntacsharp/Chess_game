5c62ccafe08ba9623579f38ae9d839313d0ed693
friedeggs01 <nhokngok.online14203@gmail.com>
2023-06-25 17:53:40 +0700

    background sound

---
+++ src/com/chess/sound/BackgroundMusic.java
@@ -0,0 +1,37 @@
+package com.chess.sound;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
+public class BackgroundMusic {
+    private Clip clip;
+
+    public BackgroundMusic(String filename) {
+        try {
+            File musicFile = new File(filename);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(musicFile);
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        if (clip != null) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+    }
+
+    public void stop() {
+        if (clip != null) {
+            clip.stop();
+            clip.close();
+        }
+    }
+}
